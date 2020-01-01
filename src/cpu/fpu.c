9c13454adf92d882da6e2c4f640ce85cb09702e6
nepx <23300488+nepx@users.noreply.github.com>
2020-01-01 15:08:17 -0800

    Fix Windows 8 BSOD
    We're only supposed to check 512 bytes of memory (0x000 to 0x1FF). However, the old code checked 513 bytes of memory (0x000 to 0x200). This was a problem when that 513th byte was on a different page

--- src/cpu/fpu.c
+++ src/cpu/fpu.c
@@ -1654,7 +1654,7 @@ int fpu_fxsave(uint32_t linaddr)
     if (fpu_nm_check())
         return 1;
 
-    if(cpu_access_verify(linaddr, linaddr + 512, cpu.tlb_shift_write)) return 1;
+    if(cpu_access_verify(linaddr, linaddr + 511, cpu.tlb_shift_write)) return 1;
     cpu_write16(linaddr + 0, fpu.control_word, cpu.tlb_shift_write);
     cpu_write16(linaddr + 2, fpu_get_status_word(), cpu.tlb_shift_write);
     // "Abridge" tag word
@@ -1700,7 +1700,7 @@ int fpu_fxrstor(uint32_t linaddr)
         EXCEPTION_GP(0);
     if (fpu_nm_check())
         return 1;
-    if(cpu_access_verify(linaddr, linaddr + 512, cpu.tlb_shift_read)) return 1;
+    if(cpu_access_verify(linaddr, linaddr + 511, cpu.tlb_shift_read)) return 1;
 
     uint32_t mxcsr;
     cpu_read32(linaddr + 24, mxcsr, cpu.tlb_shift_read);
