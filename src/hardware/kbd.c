9631aef44fc975a9bdb7f588dba95d159fa47d52
nepx <23300488+nepx@users.noreply.github.com>
2020-03-27 23:57:42 -0700

    Add mouse command 0xEA, now 9Front boots

--- src/hardware/kbd.c
+++ src/hardware/kbd.c
@@ -457,6 +457,8 @@ static void kbd_write(uint32_t port, uint32_t data)
                     kbd_add(AUX_QUEUE, kbd.mouse_resolution);
                     kbd_add(AUX_QUEUE, kbd.mouse_sample_rate);
                     break;
+                case 0xEA: // Set stream mode
+                    kbd.mouse_stream_mode = 1;
                 case 0xF2: // Get mouse ID
                     kbd_add(AUX_QUEUE, 0xFA);
                     kbd_add(AUX_QUEUE, 0);
