f07407b78fd3d8cd48f2426a4548c020e7c72d17
nepx <23300488+nepx@users.noreply.github.com>
2020-05-20 11:25:23 -0700

    Support keydown and mousemove events on Win32 version

--- src/display-win32.c
+++ src/display-win32.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #include <windows.h>
+#include <windowsx.h>
 
 
 #include "util.h"
@@ -18,11 +19,214 @@ static HDC dc_dest, dc_src; // Drawing contexts
 static void* pixels;
 static int cheight, cwidth, mouse_enabled;
 static HBITMAP hBmp;
+static int lastx, lasty;
 
 enum {
     MENU_EXIT,
     MENU_SAVE_STATE
 };
+
+static inline void lparam_to_xy(LPARAM lparam, int* x, int* y){
+    *x = GET_X_LPARAM(lparam);
+    *y = GET_Y_LPARAM(lparam);
+}
+
+static void display_set_title(void)
+{
+    char buffer[1000];
+    sprintf(buffer, "Halfix x86 Emulator - "
+                    " [%d x %d] - %s",
+        cwidth, cheight,
+        mouse_enabled ? "Press ESC to release mouse" : "Right-click to capture mouse");
+    SetWindowText(hWnd, buffer);
+}
+
+static void display_capture_mouse(int yes){
+    if(yes){
+        RECT rect;
+        // Get window size and adjust it
+        GetWindowRect(hWnd, &rect);
+        ClipCursor(&rect);
+        ShowCursor(FALSE);
+    }else {
+        ClipCursor(NULL);
+        ShowCursor(TRUE);
+    }
+    mouse_enabled = yes;
+    display_set_title();
+}
+
+// Converts a Win32 virtual key code to a PS/2 scan code.
+// See "sdl_keysym_to_scancode" in display.c for a similar implementation
+// https://stanislavs.org/helppc/make_codes.html
+static int win32_to_scancode(int w){
+    switch(w){
+        case VK_BACK:
+            return 0x0E;
+        case VK_CAPITAL:
+            return 0x3A;
+        case VK_RETURN:
+            return 0x1C;
+        case VK_ESCAPE:
+            return 0x01;
+        case VK_MENU: // ALT
+            return 0x38; // using left alt
+        case VK_CONTROL:
+            return 0x1D; // using left ctrl
+        case VK_LSHIFT:
+            return 0x2A;
+        case VK_NUMLOCK:
+            return 0x45;
+        case VK_RSHIFT:
+            return 0x36;
+        case VK_SCROLL:
+            return 0x46;
+        case VK_SPACE:
+            return 0x39;
+        case VK_TAB:
+            return 0x0F;
+        case VK_F1 ... VK_F12:
+            return w - VK_F1 + 0x3B;
+        case VK_NUMPAD0:
+            return 0x52;
+        case VK_NUMPAD1:
+            return 0x4F;
+        case VK_NUMPAD2:
+            return 0x50;
+        case VK_NUMPAD3:
+            return 0x51;
+        case VK_NUMPAD4:
+            return 0x4B;
+        case VK_NUMPAD5:
+            return 0x4C;
+        case VK_NUMPAD6:
+            return 0x4D;
+        case VK_NUMPAD7:
+            return 0x47;
+        case VK_NUMPAD8:
+            return 0x48;
+        case VK_NUMPAD9:
+            return 0x49;
+        case VK_DECIMAL: // keypad period/del
+            return 0x53;
+        case VK_MULTIPLY: // keypad asterix/prtsc
+            return 0x37;
+        case VK_SUBTRACT: // keypad dash
+            return 0x4A;
+        case VK_ADD:
+            return 0x4E;
+
+        case 0x31 ... 0x39: // 1-9
+            return w + 2 - 0x31; 
+        case 0x30: // 0
+            return 0x0B;
+// lazy - copy-pasted from display.c with a few hackish macros
+#define toUpper(b) b - ('a' - 'A')
+        
+    case toUpper('a'):
+        return 0x1E;
+    case toUpper('b'):
+        return 0x30;
+    case toUpper('c'):
+        return 0x2E;
+    case toUpper('d'):
+        return 0x20;
+    case toUpper('e'):
+        return 0x12;
+    case toUpper('f'):
+        return 0x21;
+    case toUpper('g'):
+        return 0x22;
+    case toUpper('h'):
+        return 0x23;
+    case toUpper('i'):
+        return 0x17;
+    case toUpper('j'):
+        return 0x24;
+    case toUpper('k'):
+        return 0x25;
+    case toUpper('l'):
+        return 0x26;
+    case toUpper('m'):
+        return 0x32;
+    case toUpper('n'):
+        return 0x31;
+    case toUpper('o'):
+        return 0x18;
+    case toUpper('p'):
+        return 0x19;
+    case toUpper('q'):
+        return 0x10;
+    case toUpper('r'):
+        return 0x13;
+    case toUpper('s'):
+        return 0x1F;
+    case toUpper('t'):
+        return 0x14;
+    case toUpper('u'):
+        return 0x16;
+    case toUpper('v'):
+        return 0x2F;
+    case toUpper('w'):
+        return 0x11;
+    case toUpper('x'):
+        return 0x2D;
+    case toUpper('y'):
+        return 0x15;
+    case toUpper('z'):
+        return 0x2C;
+    case VK_OEM_MINUS: // -/_
+        return 0x0C;
+    case VK_OEM_PLUS: // +/=
+        return 0x0D;
+    case VK_OEM_4: // {/[
+        return 0x1A;
+    case VK_OEM_6: // }/]
+        return 0x1B;
+    case VK_OEM_5: // \/|
+        return 0x2B;
+    case VK_OEM_1: // ;/:
+        return 0x27;
+    case VK_OEM_7: // '/"
+        return 0x28;
+    case VK_OEM_3: // `/~
+        return 0x2C;
+    case VK_OEM_COMMA: // ,/<
+        return 0x33;
+    case VK_OEM_PERIOD: // ./>
+        return 0x34;
+    case VK_OEM_2: // / or ?
+        return 0x35;
+
+    case VK_DELETE:
+        return 0xE053;
+    case VK_DOWN:
+        return 0xE050;
+    case VK_END:
+        return 0xE04F;
+    case VK_HOME:
+        return 0xE047;
+    case VK_INSERT:
+        return 0xE052;
+    case VK_LEFT:
+        return 0xE048;
+    case VK_PRIOR: // pgdn
+        return 0xE049;
+    case VK_NEXT: // pgup
+        return 0xE051;
+    case VK_RIGHT:
+        return 0xE04D;
+    case VK_UP:
+        return 0xE048;
+    }
+}
+
+static inline void display_kbd_send_key(int k)
+{
+    if (k & 0xFF00)
+        kbd_add_key(k >> 8);
+    kbd_add_key(k & 0xFF);
+}
 
 static LRESULT CALLBACK display_callback(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
@@ -34,11 +238,33 @@ static LRESULT CALLBACK display_callback(HWND hwnd, UINT msg, WPARAM wparam, LPA
             printf("Exiting.\n");
             exit(0);
             break;
-        case WM_KEYDOWN:
+        case WM_KEYDOWN: 
+            if(wparam == VK_ESCAPE && mouse_enabled) 
+                display_capture_mouse(0);
+            else 
+                display_kbd_send_key(win32_to_scancode(wparam));
             printf("Key down!!\n");
             break;
         case WM_KEYUP:
             printf("Key up!!\n");
+            break;
+        case WM_MOUSEMOVE:
+            printf("Mouse move!!\n");
+            if(mouse_enabled){
+                // Windows gives us absolute coordinates, so we have to do the calculations ourselves
+                int x, y;
+                lparam_to_xy(lparam, &x, &y);
+                int dx = x - lastx, dy = y - lasty;
+                lastx = x;
+                lasty = y;
+                kbd_send_mouse_move(dx,dy);
+            }
+            break;
+        case WM_RBUTTONDOWN:
+            if(!mouse_enabled) display_capture_mouse(1);
+            else{
+                lparam_to_xy(lparam, &lastx, &lasty);
+            }
             break;
         case WM_COMMAND:
             switch(LOWORD(wparam)){
@@ -71,16 +297,6 @@ static LRESULT CALLBACK display_callback(HWND hwnd, UINT msg, WPARAM wparam, LPA
             }
     }
     return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
-static void display_set_title(void)
-{
-    char buffer[1000];
-    sprintf(buffer, "Halfix x86 Emulator - "
-                    " [%d x %d] - %s",
-        cwidth, cheight,
-        mouse_enabled ? "Press ESC to release mouse" : "Right-click to capture mouse");
-    SetWindowText(hWnd, buffer);
 }
 
 void display_init(void) {
