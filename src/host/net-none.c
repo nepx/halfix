e18566f81e3133ce4edb9df04d4d80a4cb6a21b1
nepx <23300488+nepx@users.noreply.github.com>
2020-08-09 15:41:15 -0700

    forgot to add files with last commit

---
+++ src/host/net-none.c
@@ -0,0 +1,16 @@
+#include "util.h"
+
+int net_init(char* netarg)
+{
+    UNUSED(netarg);
+    return -1;
+}
+int net_send(void* req, int reqlen)
+{
+    UNUSED(req);
+    UNUSED(reqlen);
+    return -1;
+}
+void net_poll(void)
+{
+}
